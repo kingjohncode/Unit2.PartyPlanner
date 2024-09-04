b5609b8b3ef09293769f91fbbc0423a59769fd55
kingjohncode <jkng237@gmail.com>
2024-09-03 20:54:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+const list = document.getElementById("parties");
+const item = document.createElement("li");
+item.textContent = "test"
+list.appendChild(item)
 
+//API URLs
+const COHART = "2406-FTB-MT-WEB-PT"
+const API_URL = d-2aa9294fe819.herokuapp.com/api/$(COHART)";
+
+
+const state = {
+    parties:[]
+}
+
+async function getParties(){
+    try { 
+
+    }
+    
+}
+
+function renderParties(){
+
+
+}
